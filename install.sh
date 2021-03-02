a12137894a41c3f99cfcb32b0530fd0c5a07e1dc
Johan Lokna <jlokna@student-net.ethz.ch>
2021-03-02 17:00:19 +0100

    asd

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 pip install --upgrade pip
 
 # Install normal requirements
-pip install -r requirements.txt
+pip install -r ./requirements.txt
 
 # Install torch geometric requirements
-version=$(python get_pytorch_geometric_version.py)
-pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-${version}.html
-pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-${version}.html
-pip install torch-geometric
+# version=$(python get_pytorch_geometric_version.py)
+# pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-${version}.html
+# pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-${version}.html
+# pip install torch-geometric
