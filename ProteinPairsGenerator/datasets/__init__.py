2f204e35bd0ca7e4e0ec1eccce738eb001ad5814
Johan Lokna <jlokna@student-net.ethz.ch>
2021-03-04 23:13:59 +0100

    Bug fix

--- ProteinPairsGenerator/datasets/__init__.py
+++ ProteinPairsGenerator/datasets/__init__.py
@@ -1,3 +1,2 @@
 from .pdb_dataset import ProteinInMemoryDataset as PDBDataset
-# from .utils import *
-# from .in_memory import ProteinInMemoryDataset as OldDataset
+from .in_memory import ProteinInMemoryDataset as OldDataset
