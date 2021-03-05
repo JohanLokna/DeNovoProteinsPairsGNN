8243fe269b851837305f4fd935e36f20bd261bda
Johan Lokna <jlokna@student-net.ethz.ch>
2021-03-05 10:16:55 +0100

    PDB Dataset

--- ProteinPairsGenerator/datasets/pdb_dataset.py
+++ ProteinPairsGenerator/datasets/pdb_dataset.py
@@ -15,10 +15,11 @@ def base(data_pdb: AtomGroup) -> Data:
     # Find intersequence distance
     n = len(seq)
 
-    print(n, seq)
-
     ids = torch.arange(0, n, dtype=torch.float32).unsqueeze(0).unsqueeze(0)
+    print(ids)
     seq_distances = torch.cdist(ids, ids).flatten()
+
+    print(seq_distances)
 
     # Compute caresian distances
     coords = torch.from_numpy(data_pdb.getCoords())
