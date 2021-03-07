76b99c9d68cae3f170899de7f044f58206f00019
Johan Lokna <jlokna@student-net.ethz.ch>
2021-03-07 14:51:46 +0100

    PDB Dataset

--- ProteinPairsGenerator/datasets/pdb_dataset.py
+++ ProteinPairsGenerator/datasets/pdb_dataset.py
@@ -17,6 +17,8 @@ def base(data_pdb: AtomGroup) -> Data:
         seq_to_torch(data_pdb.getSequence()), dtype=torch.long
     )
 
+    print(data_pdb.getSequence(), end='\n\n')
+
     # Find intersequence distance
     n = seq.shape[0]
 
