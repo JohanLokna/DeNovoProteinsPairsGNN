93f4629b262af960eef812d94f6f13c43d727fb8
Johan Lokna <jlokna@student-net.ethz.ch>
2021-03-08 13:34:59 +0100

    PDB Dataset

--- ProteinPairsGenerator/datasets/pdb_dataset.py
+++ ProteinPairsGenerator/datasets/pdb_dataset.py
@@ -15,7 +15,7 @@ def base(data_pdb: AtomGroup) -> Data:
     set_pdb = data_pdb.select("name CA chain L")
     set_pdbH = data_pdb.select("name CA chain H")
     set_pdbHL = data_pdb.select("name CA chain LH")
-    print(set_pdb.getSequence() + set_pdbH..getSequence(), set_pdbLH.getSequence(), sep='\n')
+    print(set_pdb.getSequence() + set_pdbH.getSequence(), set_pdbLH.getSequence(), sep='\n')
     
     # Get sequence
     seq = torch.tensor(
