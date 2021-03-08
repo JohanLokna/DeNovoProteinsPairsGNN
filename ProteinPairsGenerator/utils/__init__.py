f310392ab487ad903ef93c446378b4de3c78960e
Johan Lokna <jlokna@student-net.ethz.ch>
2021-03-08 18:40:33 +0100

    asd

--- ProteinPairsGenerator/utils/__init__.py
+++ ProteinPairsGenerator/utils/__init__.py
@@ -1,3 +1,3 @@
-from .amino_acids import AMINO_ACIDS, AMINO_ACID_NULL, AMINO_ACIDS_ORD, AMINO_ACIDS_MAP, seq_to_tensor
+from .amino_acids import *
 from .design_protein import design_protein
 from .cdr import *
