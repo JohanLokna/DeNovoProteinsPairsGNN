5b07fc3dc771e855a482ae5267c90b4cb1dc8f8b
Johan Lokna <jlokna@student-net.ethz.ch>
2021-03-09 15:45:18 +0100

    CDR

--- ProteinPairsGenerator/utils/cdr.py
+++ ProteinPairsGenerator/utils/cdr.py
@@ -63,7 +63,7 @@ def getHeavyCDR(seq : str):
     problem = Problem()
     problem.addVariable("H1_start", range(22, 30))
     problem.addVariable("H1_size", range(10, 12 + 1))
-    problem.addVariable("H2_size", range(16, 19 + 1))
+    problem.addVariable("H2_size", range(9, 12 + 1))
     problem.addVariable("H3_size", range(2, 25 + 1))
 
     # Add constraints for H1
