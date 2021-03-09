bc96244540986fb4c7bdffff238b19ee6aa1febe
Johan Lokna <jlokna@student-net.ethz.ch>
2021-03-09 15:32:26 +0100

    SABDAB

--- ProteinPairsGenerator/datasets/sabdab_dataset.py
+++ ProteinPairsGenerator/datasets/sabdab_dataset.py
@@ -80,4 +80,4 @@ class SAbDabInMemoryDataset(PDBInMemoryDataset):
         concat = lambda x: x.dropna().tolist()
         summary = summary.groupby(summary["pdb"]).agg({"Hchain": concat, "Lchain": concat})
 
-        super().__init__(root=root, pdbs=summary, pre_transform=pre_transform  **kwargs)
+        super().__init__(root=root, pdbs=summary, pre_transform=pre_transform, **kwargs)
