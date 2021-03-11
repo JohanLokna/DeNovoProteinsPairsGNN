0675b1909d1f13261770550ca7dd8e4b7e40b370
Johan Lokna <jlokna@student-net.ethz.ch>
2021-03-11 11:55:33 +0100

    CDR

--- ProteinPairsGenerator/utils/cdr.py
+++ ProteinPairsGenerator/utils/cdr.py
@@ -1,15 +1,34 @@
 from anarci import run_anarci
+from typing import List
 
-def getCDR(seq : str, scheme : str = "chothia", hmmerpath : str = "/usr/bin/"):
+
+def getLightCDR(seq : str, scheme : str = "chothia", hmmerpath : str = "/usr/bin/"):
 
     assert scheme in ["chothia"]
 
-    renumbering = run_anarci(seq, ncpu=1, hmmerpath=hmmerpath, scheme=scheme)[1][0][0][0]
+    if scheme == "chothia":
+        renumberedTransitionPoints = iter([24, 34, 50, 56, 89, 97])
+    else:
+        raise ValueError("{} is not an implemented scheme.".format(scheme))
+    
+    return getCDR(seq, renumberedTransitionPoints, scheme, hmmerpath)
+
+
+def getHeavyCDR(seq : str, scheme : str = "chothia", hmmerpath : str = "/usr/bin/"):
+
+    assert scheme in ["chothia"]
 
     if scheme == "chothia":
         renumberedTransitionPoints = iter([26, 32, 52, 56, 95, 102])
     else:
         raise ValueError("{} is not an implemented scheme.".format(scheme))
+    
+    return getCDR(seq, renumberedTransitionPoints, scheme, hmmerpath)
+
+
+def getCDR(seq : str, renumberedTransitionPoints : List[int], scheme : str = "chothia",  hmmerpath : str = "/usr/bin/"):
+
+    renumbering = run_anarci(seq, ncpu=1, hmmerpath=hmmerpath, scheme=scheme)[1][0][0][0]
     
     cdrRenumbered = []
     pattern = next(renumberedTransitionPoints)
