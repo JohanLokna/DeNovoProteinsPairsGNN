5fa83bd392be113970d702e75a3d903de8e28cd0
Johan Lokna <jlokna@student-net.ethz.ch>
2021-03-11 11:21:09 +0100

    CDR

--- ProteinPairsGenerator/utils/cdr.py
+++ ProteinPairsGenerator/utils/cdr.py
@@ -1,7 +1,30 @@
 from ProteinPairsGenerator.utils import tensor_to_seq
+from anarci import run_anarci
 
-def getLightCDR(seq : str):
-    yield slice(20, 108)
+def getCDR(seq : str, scheme : str = "chothia", hmmerpath : str = "/usr/bin/"):
 
-def getHeavyCDR(seq : str):
-    yield slice(20, 130)
+    assert scheme in ["chothia"]
+
+    renumbering = run_anarci(seq, ncpu=1, hmmerpath="/usr/bin/", scheme=scheme)[1][0][0][0]
+
+    renumbered_points = iter([26, 32, 52, 56, 95, 102])
+    seq_points = []
+    
+    pattern = next(renumbered_points)
+    for i, x in enumerate(renumbering):
+        if pattern == x[0][0][0][0]:
+            seq_points.append(i)
+            try:
+                pattern = next(renumbered_points)
+            except StopIteration:
+                break
+
+    cdr_points = iter(seq_points)
+    while True:
+        try:
+            start = next(cdr_points)
+            end = next(cdr_points)
+        except StopIteration:
+            break
+        
+        yield slice(start, end)
