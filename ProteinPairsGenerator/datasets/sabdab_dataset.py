dd28662f63388924a0b8daa11eefea2d526d2b7d
Johan Lokna <jlokna@student-net.ethz.ch>
2021-03-16 15:14:43 +0100

    Testshape

--- ProteinPairsGenerator/datasets/sabdab_dataset.py
+++ ProteinPairsGenerator/datasets/sabdab_dataset.py
@@ -23,6 +23,8 @@ def cdr_extracter(data_pdb: AtomGroup, Lchain: List[str] = [], Hchain: List[str]
         seq = seq_to_tensor(set_pdb.getSequence())
         y = seq.clone().detach()
 
+        print((y == AMINO_ACID_NULL).any())
+
         # Mask CDR in light chains
         for c in Lchain:
           idx = Select().getIndices(set_pdb, "chain {}".format(c))
