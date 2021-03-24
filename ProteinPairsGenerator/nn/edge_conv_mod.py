3f30f66e81b973c96f90c6b38e04249a5283f9cd
Johan Lokna <jlokna@student-net.ethz.ch>
2021-03-24 22:33:56 +0100

    asd

--- ProteinPairsGenerator/nn/edge_conv_mod.py
+++ ProteinPairsGenerator/nn/edge_conv_mod.py
@@ -23,7 +23,7 @@ class EdgeConvMod(torch.nn.Module):
             out = torch.cat([x[row], x[col]], dim=-1)
         else:
             out = torch.cat([x[row], x[col], edge_attr], dim=-1)
-        print(out.shape)
+        print("Out shape: ", out.shape)
         out = self.nn(out)
         x = scatter_(self.aggr, out, row, dim_size=x.size(0))
 
