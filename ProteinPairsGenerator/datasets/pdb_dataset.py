ac2556d2f30b50f56f5faab1ec4621fa86df8da0
Johan Lokna <jlokna@student-net.ethz.ch>
2021-03-29 23:36:02 +0200

    a

--- ProteinPairsGenerator/datasets/pdb_dataset.py
+++ ProteinPairsGenerator/datasets/pdb_dataset.py
@@ -192,7 +192,7 @@ class CDRInMemoryDataset(PDBInMemoryDataset):
         )
 
         super().__init__(root=root, 
-                         pdbs=summary.head(10),
+                         pdbs=summary,
                          pre_filter=pre_filter,
                          pre_transform=pre_transform,
                          **kwargs)
