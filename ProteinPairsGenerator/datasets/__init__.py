fc2266dbfd785aa8d65cc8562c105e3aa4d116d2
Johan Lokna <jlokna@student-net.ethz.ch>
2021-03-29 12:17:56 +0200

    a

--- ProteinPairsGenerator/datasets/__init__.py
+++ ProteinPairsGenerator/datasets/__init__.py
@@ -1,3 +1,1 @@
-from .pdb_dataset import PDBInMemoryDataset
-from .sabdab_dataset import SAbDabInMemoryDataset
-from .in_memory import ProteinInMemoryDataset as ProteinInMemoryDataset
+from .pdb_dataset import PDBInMemoryDataset, CDRInMemoryDataset
