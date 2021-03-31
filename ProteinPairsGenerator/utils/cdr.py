5f41df650f666335251ff98cfffb9be0b847d51a
Johan Lokna <jlokna@student-net.ethz.ch>
2021-03-31 16:19:38 +0200

    a

--- ProteinPairsGenerator/utils/cdr.py
+++ ProteinPairsGenerator/utils/cdr.py
@@ -30,7 +30,7 @@ def getHeavyCDR(seq : str, scheme : str = "chothia", hmmerpath : str = "/usr/bin
 
 def getCDR(seq : str, renumberedTransitionPoints : List[int], scheme : str = "chothia",  hmmerpath : str = "/usr/bin/"):
 
-    print(hmmerpath)
+    print("CDR general: " + hmmerpath)
     renumbering = run_anarci(seq, ncpu=1, hmmerpath=hmmerpath, scheme=scheme)[1][0][0][0]
     
     cdrRenumbered = []
