4964c3a4aebaed64e6c4b2de0f4265267baba8c3
Johan Lokna <jlokna@student-net.ethz.ch>
2021-04-10 18:00:08 +0200

    a

--- ProteinPairsGenerator/IngrahamModel/utils.py
+++ ProteinPairsGenerator/IngrahamModel/utils.py
@@ -13,20 +13,24 @@ def makeData(
         start = time.time()
         for i, (pdbName, _) in enumerate(pdbDf.iterrows()):
 
-            entry = {}
+            try:
+                entry = {}
 
-            fetchPDBviaHTTP(pdbName, compressed=True)
-            pdb = parsePDB(pdbName)
+                fetchPDBviaHTTP(pdbName, compressed=True)
+                pdb = parsePDB(pdbName)
 
-            entry["seq"] = pdb.ca.getSequence()
-            entry["name"] = pdbName
+                entry["seq"] = pdb.ca.getSequence()
+                entry["name"] = pdbName
 
-            # Convert raw coords to np arrays
-            entry["coords"] = {}
-            for atom in coordAtoms:
-                entry["coords"][atom] = pdb.select("name {}".format(atom)).getCoordsets()[0].tolist()
+                # Convert raw coords to np arrays
+                entry["coords"] = {}
+                for atom in coordAtoms:
+                    entry["coords"][atom] = pdb.select("name {}".format(atom)).getCoordsets()[0].tolist()
 
-            data.append(entry)
+                data.append(entry)
+
+            except Exception as e:
+                print(e)
 
             if verbose and (i + 1) % 1000 == 0:
                 elapsed = time.time() - start
