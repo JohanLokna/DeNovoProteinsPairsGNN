f70f40a2a476b37950421362eb923b12fc9b4dcd
Johan Lokna <jlokna@student-net.ethz.ch>
2021-04-11 23:55:19 +0200

    a

--- ProteinPairsGenerator/datasets/compute_modules.py
+++ ProteinPairsGenerator/datasets/compute_modules.py
@@ -24,6 +24,7 @@ def helperComputeModuledef(argList : List, f, identifier = None):
             value = f(**x)
         except Exception:
             value = None
+        print(x.keys(), identifier)
         # data[i if identifier is None else x[identifier]] = value
     return data
 
