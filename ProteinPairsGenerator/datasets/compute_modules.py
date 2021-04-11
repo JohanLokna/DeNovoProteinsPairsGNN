22c8964185f2f8290cac121b87c564758f171e82
Johan Lokna <jlokna@student-net.ethz.ch>
2021-04-11 23:36:39 +0200

    a

--- ProteinPairsGenerator/datasets/compute_modules.py
+++ ProteinPairsGenerator/datasets/compute_modules.py
@@ -57,6 +57,7 @@ class ComputeModule:
         torch.save({self.featureName: self.data}, self.filename if filename is None else filename)
 
     def __call__(self, argList : List, identifier = None, pool : Union[Pool, None] = None):
+      print(self.__dict__)
       if pool is None:
           self.data = helperComputeModuledef(argList, identifier)
       else:
