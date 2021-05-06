4e4ad6b3a3b9eface297ed8a39c97a394bb70d13
Johan Lokna <jlokna@student-net.ethz.ch>
2021-05-07 00:13:51 +0200

    a

---
+++ ProteinPairsGenerator/DistilationKnowledge/kdModel.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+
+def getKDModel(baseModel):
+
+    class KDModel(baseModel):
+
+        def __init__(
+            self, 
+            alpha : float,
+            *args, 
+            **kwargs
+        ) -> None:
+            super().__init__(*args, **kwargs)
+            self.alpha = alpha
+            self.output = None
+            self.criterion = nn.CrossEntropyLoss(ignore_index=-1)
+
+        def step(self, batch):
+            outDict = super().__call__(batch)
+            outDict["loss"] = self.alpha * outDict["loss"] \
+                            + (1 - self.alpha) * self.criterion(self.output, batch.teacherSeq)
+            return outDict
+
+        def __call__(self, *args, **kwargs):
+            self.output = super().__call__(*args, **kwargs)
+            return self.output
+
+    return KDModel
