0164c6b03abd24ab74d33cd29afb1400fff1d248
Johan Lokna <jlokna@student-net.ethz.ch>
2021-05-07 13:22:05 +0200

    a

--- ProteinPairsGenerator/DistilationKnowledge/kdModel.py
+++ ProteinPairsGenerator/DistilationKnowledge/kdModel.py
@@ -26,7 +26,7 @@ def getKDModel(baseModel : pl.LightningModule, alpha : float):
 
         def step(self, x):
             outDict = super().step(x)
-            teacherLabels = (extractBaseAcids(self.teacher(x.tokenizedSeq)[0]))
+            teacherLabels = extractBaseAcids(self.teacher(x.tokenizedSeq)[0])
             lossTeacher = torch.mean(torch.sum(-log_softmax(self.output, dim=1) * softmax(teacherLabels, dim=1), dim=1)[x.mask])
             outDict["loss"] = self.alpha * outDict["loss"] + (1 - self.alpha) * lossTeacher
             return outDict
