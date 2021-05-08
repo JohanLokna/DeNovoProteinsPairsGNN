c60b3721e27940ac24b193f71a0fed93053c7efa
Johan Lokna <jlokna@student-net.ethz.ch>
2021-05-08 16:52:31 +0200

    a

--- ProteinPairsGenerator/DistilationKnowledge/kdModel.py
+++ ProteinPairsGenerator/DistilationKnowledge/kdModel.py
@@ -3,34 +3,33 @@ import torch.nn as nn
 from torch.nn.functional import softmax, log_softmax
 import pytorch_lightning as pl
 
-from .utils import extractBaseAcids
 
-def getKDModel(baseModel : pl.LightningModule, alpha : float):
+# def getKDModel(baseModel : pl.LightningModule, alpha : float):
 
-    class KDModel(baseModel):
+#     class KDModel(baseModel):
 
-        def __init__(
-            self,
-            *args, 
-            **kwargs
-        ) -> None:
-            super().__init__(*args, **kwargs)
-            self.alpha = alpha
-            self.output = None
-            self.softmax = nn.Softmax(dim=1)
-            self.teacher = ProteinBertForMaskedLM.from_pretrained('bert-base')
-            for param in self.teacher.parameters():
-                param.requires_grad = False
+#         def __init__(
+#             self,
+#             *args, 
+#             **kwargs
+#         ) -> None:
+#             super().__init__(*args, **kwargs)
+#             self.alpha = alpha
+#             self.output = None
+#             self.softmax = nn.Softmax(dim=1)
+#             self.teacher = ProteinBertForMaskedLM.from_pretrained('bert-base')
+#             for param in self.teacher.parameters():
+#                 param.requires_grad = False
 
-        def step(self, x):
-            outDict = super().step(x)
-            teacherLabels = extractBaseAcids(self.teacher(x.tokenizedSeq)[0])
-            lossTeacher = torch.mean(torch.sum(-log_softmax(self.output, dim=1) * softmax(teacherLabels, dim=1), dim=1)[x.mask])
-            outDict["loss"] = self.alpha * outDict["loss"] + (1 - self.alpha) * lossTeacher
-            return outDict
+#         def step(self, x):
+#             outDict = super().step(x)
+#             teacherLabels = extractBaseAcids(self.teacher(x.tokenizedSeq)[0])
+#             lossTeacher = torch.mean(torch.sum(-log_softmax(self.output, dim=1) * softmax(teacherLabels, dim=1), dim=1)[x.mask])
+#             outDict["loss"] = self.alpha * outDict["loss"] + (1 - self.alpha) * lossTeacher
+#             return outDict
 
-        def __call__(self, *args, **kwargs):
-            self.output = super().__call__(*args, **kwargs)
-            return self.output
+#         def __call__(self, *args, **kwargs):
+#             self.output = super().__call__(*args, **kwargs)
+#             return self.output
 
-    return KDModel
+#     return KDModel
