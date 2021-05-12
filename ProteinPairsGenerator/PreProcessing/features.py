86a821a68f868a9e14774e32245260b23724c37b
Johan Lokna <jlokna@student-net.ethz.ch>
2021-05-12 09:45:38 +0200

    a

--- ProteinPairsGenerator/PreProcessing/features.py
+++ ProteinPairsGenerator/PreProcessing/features.py
@@ -574,9 +574,7 @@ class TAPEFeatures(FeatureModule):
         *args,
         **kwargs
     ) -> torch.Tensor:
-        a = [self.annotator([seq])[0] for seq, _ in self.dependencies[0].data]
-        print(a)
-        return a
+        return [self.annotator([seq])[0] for seq, _ in self.dependencies[0].data]
         
     def preFilter(self, *args, **kwargs) -> bool:
         return all([d.preFilter(*args, **kwargs) for d  in self.dependencies])
