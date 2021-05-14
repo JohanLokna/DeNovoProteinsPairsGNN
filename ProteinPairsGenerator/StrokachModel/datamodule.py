b8869b49b9668393b71ec68752ae8003ec058f7a
Johan Lokna <jlokna@student-net.ethz.ch>
2021-05-14 14:14:51 +0200

    .

--- ProteinPairsGenerator/StrokachModel/datamodule.py
+++ ProteinPairsGenerator/StrokachModel/datamodule.py
@@ -32,13 +32,12 @@ class StrokachDataModule(pl.LightningDataModule):
         pass
 
     def train_dataloader(self):
-        return StrokachLoader(self.dataset.getSubset(
-            self.trainSet, 
-            teacher=self.teacher,
+        return StrokachLoader(
+            self.dataset.getSubset(self.trainSet), 
             teacher=self.teacher,
             num_workers=8, 
             pin_memory=True
-        ))
+        )
 
     def val_dataloader(self):
         return StrokachLoader(self.dataset.getSubset(self.valSet), teacher=self.teacher)
