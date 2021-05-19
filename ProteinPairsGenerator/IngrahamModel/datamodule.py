0c58d6775b69e71d1c7ceced60d7232f7710bd53
Johan Lokna <jlokna@student-net.ethz.ch>
2021-05-19 12:34:28 +0200

    .

--- ProteinPairsGenerator/IngrahamModel/datamodule.py
+++ ProteinPairsGenerator/IngrahamModel/datamodule.py
@@ -4,4 +4,5 @@ from .loader import IngrahamLoader
 class IngrahamDataModule(BERTDataModule):
 
     def __init__(self, *args, **kwargs):
+          print(kwargs)
           super().__init__(loaderClass=IngrahamLoader, *args, **kwargs)
