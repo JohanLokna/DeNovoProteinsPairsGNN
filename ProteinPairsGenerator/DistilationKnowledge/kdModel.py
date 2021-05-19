d4ba963d5ea15d949a15f546ca95fdcab4476e71
Johan Lokna <jlokna@student-net.ethz.ch>
2021-05-19 13:49:26 +0200

    .

--- ProteinPairsGenerator/DistilationKnowledge/kdModel.py
+++ ProteinPairsGenerator/DistilationKnowledge/kdModel.py
@@ -5,7 +5,7 @@ from torch.nn.functional import softmax, log_softmax
 import pytorch_lightning as pl
 
 
-def getKDModel(baseModel : pl.LightningModule):
+def getKDModel(baseModel : pl.LightningModule, classDim : int):
 
     class KDModel(baseModel):
 
@@ -18,18 +18,17 @@ def getKDModel(baseModel : pl.LightningModule):
             super().__init__(*args, **kwargs)
             self.alpha = alpha
             self.output = None
+            self.classDim = classDim
 
         def step(self, x):
             outDict = super().step(x)
 
-            print(self.output.shape)
             #lossTeacher = torch.mean(torch.sum(-log_softmax(self.output, dim=1) * softmax(x.teacherLabels, dim=1), dim=1)[x.mask])
 
-            lossTeacher = torch.sum(-log_softmax(self.output, dim=1) * softmax(x.teacherLabels, dim=1), dim=1)
-            print(lossTeacher.shape, x.mask.shape, sep="\n")
+            lossTeacher = torch.sum(-log_softmax(self.output, dim=self.classDim) \
+                                   * softmax(x.teacherLabels, dim=self.classDim), dim=self.classDim)
             lossTeacher = lossTeacher[x.mask] if x.mask.dtype in [torch.long, torch.bool] else lossTeacher * x.mask
             lossTeacher = torch.mean(lossTeacher)
-
             outDict["loss"] = (1 - self.alpha) * outDict["loss"] + self.alpha * lossTeacher
             return outDict
 
