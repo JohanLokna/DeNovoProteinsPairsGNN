0065274abffd597caf8cf9d9e0ebea8fcfc07d0a
Johan Lokna <jlokna@student-net.ethz.ch>
2021-05-22 13:38:32 +0200

    .

--- ProteinPairsGenerator/JLoModel/model.py
+++ ProteinPairsGenerator/JLoModel/model.py
@@ -11,19 +11,23 @@ from ProteinPairsGenerator.StrokachModel import StrokachModel
 
 class BERTHelper(nn.Module):
 
-    def __init__(self):
+    def __init__(self, hidden_size : int):
         super().__init__()
         self.model = ProteinBertModel.from_pretrained('bert-base')
+        self.encode = nn.Sequential(
+            nn.Linear(768, hidden_size),
+            nn.LayerNorm(hidden_size)
+        )
 
     def forward(self, x):
-        return self.model(x.unsqueeze(0))[0][0, 1:-1]
+        return self.encode(self.model(x.unsqueeze(0))[0][0, 1:-1])
 
 
 class Net(StrokachModel):
     def __init__(
-        self, 
+        self,
         *args,
         **kwargs
     ) -> None:
         super().__init__(*args, **kwargs)
-        self.embed_x = BERTHelper()
+        self.embed_x = BERTHelper(kwargs["hidden_size"])
