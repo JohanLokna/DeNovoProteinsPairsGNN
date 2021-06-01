472bffcb0d4321b3db211418209ef3c3cc58015e
Johan Lokna <jlokna@student-net.ethz.ch>
2021-06-01 17:34:43 +0200

    .

--- ProteinPairsGenerator/StrokachModel/__init__.py
+++ ProteinPairsGenerator/StrokachModel/__init__.py
@@ -1,1 +1,3 @@
-from .testLevels import *
+from .model import Net as StrokachModel
+from .datamodule import StrokachDataModule
+from .loader import StrokachLoader
--- ProteinPairsGenerator/Testing/__init__.py
+++ ProteinPairsGenerator/Testing/__init__.py
@@ -1,3 +1,1 @@
-from .model import Net as StrokachModel
-from .datamodule import StrokachDataModule
-from .loader import StrokachLoader
+from .testLevels import *
