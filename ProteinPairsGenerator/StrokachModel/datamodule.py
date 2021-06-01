44485601e5e097d6bd050f15fb80fcd73e35c291
Johan Lokna <jlokna@student-net.ethz.ch>
2021-06-01 23:54:58 +0200

    .

--- ProteinPairsGenerator/StrokachModel/datamodule.py
+++ ProteinPairsGenerator/StrokachModel/datamodule.py
@@ -1,7 +1,7 @@
 from ProteinPairsGenerator.Data import BERTDataModule
 from .loader import StrokachLoader
 
-class IngrahamDataModule(BERTDataModule):
+class StrokachDataModule(BERTDataModule):
 
     def __init__(self, *args, **kwargs):
           kwargs["loaderClass"] = StrokachLoader
