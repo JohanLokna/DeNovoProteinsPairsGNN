09ad93dad99962f094254c1596929eee20517fa4
Johan Lokna <jlokna@student-net.ethz.ch>
2021-06-09 23:00:48 +0200

    .

---
+++ ProteinPairsGenerator/PostProcessing/__init__.py
@@ -0,0 +1,1 @@
+from .simpleCorrector import *
---
+++ ProteinPairsGenerator/PostProcessing/simpleCorrector.py
@@ -0,0 +1,29 @@
+# General import
+from tape import ProteinBertForMaskedLM
+
+import torch
+from torch import nn
+
+from ProteinPairsGenerator.BERTModel import AdaptedTAPETokenizer
+
+# Pytorch imports
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+
+
+def getCorrectorModel(baseModel : pl.LightningModule, classDim : int):
+
+    class CorrectorModel(baseModel):
+        
+        def __init__(self, *args, **kwargs):
+            self.tokenizer = AdaptedTAPETokenizer()
+            self.corrector = ProteinBertForMaskedLM.from_pretrained('bert-base')
+            super().__init__(*args, **kwargs)
+
+        def forward(self, x):
+            preds = torch.argmax(super().__call__(x).data, 1)
+            corrPreds = self.corrector(self.tokenizer.AA2BERT(preds))
+            return self.tokenizer.BERT2AA(corrPreds)
+
+    return CorrectorModel
