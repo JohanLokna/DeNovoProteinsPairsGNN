d4c57910fc5ac4efeb30f5907d4aeecbf916e65c
Johan Lokna <jlokna@student-net.ethz.ch>
2021-06-18 13:41:55 +0200

    .

--- scripts/hpStrokach.py
+++ scripts/hpStrokach.py
@@ -3,32 +3,25 @@ from pathlib import Path
 import shutil
 import os
 
-def runStorkach(hidden_size, N, alpha, lr, 
-                modelConfig : Path = Path("config.yaml"), baseConfig : Path = Path("../config.yaml"),
-                runFile : Path = Path("../../run.py")):
-
-    # Fix to int
-    hidden_size = int(2 ** hidden_size)
-    N = int(N)
+def runModel(configFile : Path = Path("config.yaml"), runFile : Path = Path("../run.py"), **kwargs):
 
     # Get name
-    name = "model:Strokach_h:{}_N:{}_alpha:{}_lr:{}".format(hidden_size, N, alpha, lr)
-    
+    name = "_".join(["{}={}".format(k, str(v)) for k, v in kwargs.items()])
+
     # Create folder structure
-    Path("Logging").mkdir(exist_ok=True)
     root = Path("Experiments").joinpath(name)
     root.mkdir(parents=True)
+    updatedConfig = root.joinpath("config.yaml")
 
-    # Create config file
-    outfile = root.joinpath("config.yaml")
-    shutil.copy(baseConfig, outfile)
-    with outfile.open("a") as out:
-        for l in modelConfig.open().readlines():
-            for k, v in [("hidden_size", hidden_size), ("N", N), ("alpha", alpha), ("lr", lr)]:
+    # Load config
+    kwargs.update({"experiment_name": name})
+    with updatedConfig.open("a") as out:
+        for l in configFile.open().readlines():
+            for k, v in kwargs.items():
                 if k + ": " in l:
-                    l = l.split(": ")[0] + ": " + str(v)
+                    l = l.split(": ")[0] + ": " + str(v) + "\n"
                     break
             out.write(l)
 
     # Run
-    os.system("cd {} ; python3 {} --config config.yaml".format(str(root), str(runFile)))
+    # os.system("cd {} ; python3 {} --config config.yaml".format(str(root), str(runFile)))
