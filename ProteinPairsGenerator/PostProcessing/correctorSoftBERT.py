135345d925c4368c6f0f6d85e10382999556a826
Johan Lokna <jlokna@student-net.ethz.ch>
2021-06-22 22:02:13 +0200

    .

--- ProteinPairsGenerator/PostProcessing/correctorSoftBERT.py
+++ ProteinPairsGenerator/PostProcessing/correctorSoftBERT.py
@@ -42,7 +42,7 @@ class CorrectorSoftBERT(pl.LightningModule):
         
         print("h dim: ", h.shape)
 
-        p = self.switch(h)
+        p = self.switch(h).squeeze(-1)
 
         print("p dim: ", p.shape)
         print("m dim: ", self.masker(x).shape)
