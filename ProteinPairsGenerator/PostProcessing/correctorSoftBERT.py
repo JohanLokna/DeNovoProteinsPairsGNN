22dd906f8bde3575ea5af5728cad25787cf7c6a6
Johan Lokna <jlokna@student-net.ethz.ch>
2021-06-23 12:57:05 +0200

    .

--- ProteinPairsGenerator/PostProcessing/correctorSoftBERT.py
+++ ProteinPairsGenerator/PostProcessing/correctorSoftBERT.py
@@ -43,6 +43,9 @@ class CorrectorSoftBERT(BERTModel):
     def criterion(self, p : torch.Tensor, yHat : torch.Tensor, x : torch.Tensor, y : torch.Tensor):
         g = (x != y).type_as(p)
         # print(p.shape, g.shape, yHat.shape, y.shape)
+        print(self.bce(p, g))
+        print(self.ce(yHat.view((-1,) + yHat.shape[2:]), y.view((-1,) + y.shape[2:])))
+        print(self.alpha)
         return self.alpha * self.bce(p, g) \
              + (1 - self.alpha) * self.ce(yHat.view((-1,) + yHat.shape[2:]), y.view((-1,) + y.shape[2:]))
 
