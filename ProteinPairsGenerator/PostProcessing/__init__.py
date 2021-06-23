b51d6f184f2b5e736944ae41788de519c70c3d24
Johan Lokna <jlokna@student-net.ethz.ch>
2021-06-23 16:51:43 +0200

    .

--- ProteinPairsGenerator/PostProcessing/__init__.py
+++ ProteinPairsGenerator/PostProcessing/__init__.py
@@ -1,2 +1,4 @@
 from .simpleCorrector import *
 from .correctorSoftBERT import *
+from .correctorPipeline import *
+CorrectorFullSoftBERTPipline = getCorrectorPipeline(CorrectorFullSoftBERT)
---
+++ ProteinPairsGenerator/PostProcessing/correctorPipeline.py
@@ -0,0 +1,38 @@
+# Pytorch imports
+import torch
+import torch.nn as nn
+from torch.nn.functional import softmax, log_softmax
+import pytorch_lightning as pl
+
+# Local imports
+from ProteinPairsGenerator.Data import GeneralData
+
+
+def getCorrectorPipeline(corrector : pl.LightningModule):
+
+    class CorrectorPipeline(corrector):
+
+        def __init__(self, baseModel : nn.Module, classDim : int, *args, **kwargs) -> None:
+            super().__init__(*args, **kwargs)
+            self.classDim = classDim
+            self.baseModel = baseModel
+
+            # Freeze base model
+            for param in self.baseModel.parameters():
+                param.requires_grad = False
+
+            # Wrap base model to get output
+            self.output = None
+
+            def wrapper(*args, **kwargs):
+                baseModelOutput = type(self.baseModel).forward(*args, **kwargs)
+                self.output = torch.argmax(baseModelOutput.data, self.classDim)
+                return baseModelOutput
+
+            baseModel.forward = wrapper.__get__(baseModel, type(baseModel))
+
+        def step(self, x):
+            self.baseModel.step(x)
+            return self.step(GeneralData(x=self.output, y=x.seq))
+
+    return CorrectorPipeline
