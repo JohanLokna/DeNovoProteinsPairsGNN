b17e20c6dc91ed929d51384ee4844a92ddb42b3d
Johan Lokna <jlokna@student-net.ethz.ch>
2021-07-10 09:54:30 +0200

    .

--- ProteinPairsGenerator/JLoModel/__init__.py
+++ ProteinPairsGenerator/JLoModel/__init__.py
@@ -1,3 +1,5 @@
 from .model import Net as JLoModel
 from .datamodule import *
 from .loader import *
+from ProteinPairsGenerator.DistilationKnowledge import getKDModel
+JLoModelKD = getKDModel(JLoModel, 1)
