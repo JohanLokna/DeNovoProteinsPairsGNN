83e4b17e4b53a8fca1625b53cff1b62ec306e2fd
Johan Lokna <jlokna@student-net.ethz.ch>
2021-07-20 23:44:38 +0200

    .

--- ProteinPairsGenerator/IngrahamModel/__init__.py
+++ ProteinPairsGenerator/IngrahamModel/__init__.py
@@ -2,4 +2,4 @@ from .struct2seq import Struct2Seq as IngrahamModel
 from .loader import IngrahamLoader
 from .datamodule import IngrahamDataModule
 from ProteinPairsGenerator.DistilationKnowledge import getKDModel
-IngrahamModelKD = getKDModel(IngrahamModel, 0)
+IngrahamModelKD = getKDModel(IngrahamModel, -1)
