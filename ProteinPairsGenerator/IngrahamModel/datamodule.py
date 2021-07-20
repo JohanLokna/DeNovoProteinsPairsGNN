6644941b1c7780547245042f02dc92edeec6ed71
Johan Lokna <jlokna@student-net.ethz.ch>
2021-07-20 14:59:41 +0200

    .

--- ProteinPairsGenerator/IngrahamModel/datamodule.py
+++ ProteinPairsGenerator/IngrahamModel/datamodule.py
@@ -1,8 +1,52 @@
+# General imports
+from pathlib import Path
+from typing import Union, Optional, List
+
+# Torch imports
+import torch
+
+# Local imports
 from ProteinPairsGenerator.Data import BERTDataModule
 from .loader import IngrahamLoader
 
+def batchAccordingToSize(dataset, indices, cutoffSize = 5000):
+    newIndices = []
+
+    currCount = 0
+    currBatch = []
+
+    for idx in indices:
+        currCount += torch.numel(dataset.get(idx))
+        currBatch.append(idx)
+
+        if cutoffSize >= cutoffSize:
+            newIndices.append(currBatch)
+            currCount, currBatch = 0, []
+
+    return newIndices
+
+
 class IngrahamDataModule(BERTDataModule):
 
-    def __init__(self, *args, **kwargs):
-          kwargs["loaderClass"] = IngrahamLoader
-          super().__init__(*args, **kwargs)
+    def __init__(
+        self,
+        root : Union[Path, str],
+        trainSet: Optional[Union[Path, List[Path]]] = None,
+        valSet: Optional[List[Path]] = None,
+        testSet: Optional[List[Path]] = None,
+        teacher: Optional[str] = None,
+        num_workers: int = 1,
+        prefetch_factor: int = 2
+    ):
+          super().__init__(
+              root,
+              IngrahamLoader,
+              trainSet,
+              valSet,
+              testSet,
+              teacher,
+              num_workers,
+              prefetch_factor,
+              batch_size = 1,
+              batch_builder = batchAccordingToSize
+          )
