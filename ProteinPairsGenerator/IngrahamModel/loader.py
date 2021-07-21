f058ef7b7581ef0b9adf79570a6d8ead36cab3b9
Johan Lokna <jlokna@student-net.ethz.ch>
2021-07-21 23:02:47 +0200

    .

--- ProteinPairsGenerator/IngrahamModel/loader.py
+++ ProteinPairsGenerator/IngrahamModel/loader.py
@@ -46,9 +46,10 @@ class IngrahamLoader(BERTLoader):
             for i, (b, l) in enumerate(zip(batch, lengths)):
 
                 # Standard features
-                coords[i, :l] = torch.swapaxes(b.coords, 1, 2) / 100 # Fix this
+                coords[i, :l] = torch.transpose(b.coords, 1, 2) / 100 # Fix this
                 seq[i, :l] = b.seq
                 valid[i, :l] = 1.0
+                valid[i, :, ] = torch.zeros(3)
 
                 # Randomly selecet masked sequence
                 idx = torch.randint(len(b.maskBERT), (1,)).item()
