5cd6d1a2a9c63d3fe6580eae532521948afc4967
Johan Lokna <jlokna@student-net.ethz.ch>
2021-07-27 14:03:16 +0200

    .

--- ProteinPairsGenerator/utils/__init__.py
+++ ProteinPairsGenerator/utils/__init__.py
@@ -2,9 +2,9 @@ from .amino_acids import *
 from .cdr import *
 from .bayesianHP import *
 
-from warnings import warn
+from warnings import showwarning
 
 try:
     from .setupRun import *
 except Exception:
-    warn("Failed to import setupRun.\nCheck that all GPUs are working.\nContinuing under the assumtion that this feature is not used.")
+    showwarning("Failed to import setupRun.\nCheck that all GPUs are working.\nContinuing under the assumtion that this feature is not used.")
