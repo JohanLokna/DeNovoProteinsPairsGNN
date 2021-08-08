f1ce37f67ba84643b9acd1ed86eee97f177cd3d9
Johan Lokna <jlokna@student-net.ethz.ch>
2021-08-08 23:01:11 +0200

    .

--- ProteinPairsGenerator/PostProcessing/guessesData.py
+++ ProteinPairsGenerator/PostProcessing/guessesData.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 import json
 from random import shuffle
+from typing import Union, List, Optional, Callable
 
 import torch
+import pytorch_lightning as pl
 
 from ProteinPairsGenerator.BERTModel import AdaptedTAPETokenizer
 from ProteinPairsGenerator.Data import GeneralData
@@ -10,11 +12,12 @@ from ProteinPairsGenerator.utils import seq_to_tensor, AMINO_ACIDS_MAP
 
 class GuessDataset(torch.utils.data.IterableDataset):
 
-    def __init__(self, root : Path, xToken : str = "seq", yToken : str = "guess") -> None:
+    def __init__(self, root : Path, xToken : str = "seq", yToken : str = "guess", maskToken = "mask") -> None:
         super().__init__()
         self.tokenizer = AdaptedTAPETokenizer()
         self.xToken = xToken
         self.yToken = yToken
+        self.maskToken = maskToken
 
         self.data = []
         for l in open(root, "r").readlines():
@@ -23,16 +26,62 @@ class GuessDataset(torch.utils.data.IterableDataset):
 
             x = seq_to_tensor(jsonDict[self.xToken], AMINO_ACIDS_MAP)
             y = seq_to_tensor(jsonDict[self.yToken], AMINO_ACIDS_MAP)
+            mask = torch.BoolTensor(jsonDict[self.maskToken])
 
             self.data.append(GeneralData(
               x = self.tokenizer.AA2BERT(x)[0],
-              y = self.tokenizer.AA2BERT(y)[0]
+              y = self.tokenizer.AA2BERT(y)[0],
+              mask = mask
             ))
+        shuffle(self.data)
+
+
 
     def __iter__(self):
         return iter(self.data)
+
 
 class GuessLoader(torch.utils.data.DataLoader):
 
     def __init__(self, dataset, batch_size=1, shuffle=True, num_workers=0):
         super().__init__(dataset, batch_size, shuffle, num_workers, collate_fn=lambda x: x)
+
+
+class BERTDataModule(pl.LightningDataModule):
+
+    def __init__(
+        self,
+        trainSet: Optional[List[Path]] = None,
+        valSet: Optional[List[Path]] = None,
+        testSet: Optional[List[Path]] = None,
+        num_workers: int = 1,
+    ) -> None:
+        super().__init__()
+
+        self.trainDataset = GuessDataset(trainSet) if trainSet else None
+        self.valDataset = GuessDataset(valSet) if valSet else None
+        self.testDataset = GuessDataset(testSet) if testSet else None
+        self.num_workers = num_workers
+
+    def setup(self, stage=None):
+        pass
+
+    def train_dataloader(self):
+        return GuessLoader(
+            dataset=self.trainDataset,
+            num_workers=self.num_workers
+        )
+
+    def val_dataloader(self):
+        return GuessLoader(
+            dataset=self.trainDataset
+        )
+
+    def test_dataloader(self):
+        return GuessLoader(
+            dataset=self.trainDataset
+        )
+
+    def transfer_batch_to_device(self, x, device):
+        x.__dict__.update((k, v.to(device=device)) for k, v in x.__dict__.items() if isinstance(v, torch.Tensor))
+        return x
