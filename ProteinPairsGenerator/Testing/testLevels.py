2fc62e7a988eb51a3e559c4dbd3f4bde7b576d36
Johan Lokna <jlokna@student-net.ethz.ch>
2021-08-08 15:42:04 +0200

    .

--- ProteinPairsGenerator/Testing/testLevels.py
+++ ProteinPairsGenerator/Testing/testLevels.py
@@ -21,7 +21,7 @@ class TestProteinDesign:
 
     def run(self, model, dm, verbose = False, addRandomKD = False) -> None:
 
-        # model.to(device=self.device)
+        model.to(device=self.device)
 
         for level in self.levels:
             stepResults = []
