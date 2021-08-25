e2787ee19e6c1aa6c691f877b67c7fd0e6df575a
Johan Lokna <jlokna@student-net.ethz.ch>
2021-08-25 16:59:41 +0200

    .

--- ProteinPairsGenerator/IngrahamModel/model2.py
+++ ProteinPairsGenerator/IngrahamModel/model2.py
@@ -251,7 +251,7 @@ class Struct2Seq(BERTModel):
         for k in self.kAccuracy:
             yPred_k = torch.topk(output.data, k, -1).indices
             print(yPred_k.shape, x.seq.shape)
-            nCorrect_k = ((x.seq == yPred_k) * x.mask).sum()
+            nCorrect_k = ((x.seq.unsqueeze(-1) == yPred_k) * x.mask).sum()
             out.update({"nCorrect_{}".format(k): nCorrect_k})
 
         return out
