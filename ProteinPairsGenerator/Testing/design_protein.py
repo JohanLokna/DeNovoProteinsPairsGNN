40ae089e56652669ca50f91d9ecc41c86699c2f3
Johan Lokna <jlokna@student-net.ethz.ch>
2021-08-27 10:03:04 +0200

    .

--- ProteinPairsGenerator/Testing/design_protein.py
+++ ProteinPairsGenerator/Testing/design_protein.py
@@ -2,18 +2,18 @@ import torch
 from ProteinPairsGenerator.utils import AMINO_ACID_NULL, AMINO_ACIDS_MAP
 
 @torch.no_grad()
-def designProtein(net : torch.nn.Module, kw_x : str, dim : int, **kwargs):
+def designProtein(net : torch.nn.Module, kw_seq : str, in_seq, dim : int, **kwargs):
     
     # Can only design a single protein at the time
-    assert len(kwargs[kw_x].shape) == 1
+    assert len(in_seq.shape) == 1
 
     # Mask initially
-    mask = kwargs[kw_x] == AMINO_ACIDS_MAP[AMINO_ACID_NULL]
+    mask = in_seq == AMINO_ACIDS_MAP[AMINO_ACID_NULL]
 
     while not mask.any():
 
         # Predict based on current predictions
-        output = net(**kwargs)
+        output = net(kw_seq = in_seq, **kwargs)
 
         # Normalize predictions
         output = torch.softmax(output, dim=dim)
@@ -25,7 +25,7 @@ def designProtein(net : torch.nn.Module, kw_x : str, dim : int, **kwargs):
         _, max_residue = max_pred.max(dim=0)
 
         # Update with max prediction
-        kwargs[kw_x][max_residue] = max_index[max_residue]
+        in_seq = max_index[max_residue]
         mask[max_residue] = False
     
-    return kwargs[kw_x]
+    return in_seq
