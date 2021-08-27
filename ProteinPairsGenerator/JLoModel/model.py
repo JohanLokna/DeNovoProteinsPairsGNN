8a3beb223fd70ac8e5725e125c912c8464272f3e
Johan Lokna <jlokna@student-net.ethz.ch>
2021-08-27 14:45:36 +0200

    .

--- ProteinPairsGenerator/JLoModel/model.py
+++ ProteinPairsGenerator/JLoModel/model.py
@@ -27,9 +27,8 @@ class BERTHelper(nn.Module):
     def forward(self, x):
 
         if self.tokenizer:
-            print("OK")
             x = self.tokenizer.AA2BERT(x).type_as(x)
-            return self.encode(self.model(x)[0][:, 1:-1])
+            return self.encode(self.model(x)[0][0, 1:-1])
 
         else:
             return self.encode(self.model(x.unsqueeze(0))[0][0, 1:-1])
