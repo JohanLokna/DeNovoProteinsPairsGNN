aa40e6c1700719b33bd312e6d07a328a84f2fb81
Johan Lokna <jlokna@student-net.ethz.ch>
2021-08-27 09:44:00 +0200

    .

--- ProteinPairsGenerator/Testing/design_protein.py
+++ ProteinPairsGenerator/Testing/design_protein.py
@@ -1,14 +1,31 @@
 import torch
+from ProteinPairsGenerator.utils import AMINO_ACID_NULL, AMINO_ACIDS_MAP
 
 @torch.no_grad()
-def design_protein(net, x, edge_index, edge_attr):
-    mask = (x == 20)
-    if not mask.any():
-        return x
-    output = net(x, edge_index, edge_attr)
-    output = torch.softmax(output, dim=1)
-    max_pred, max_index = output.max(dim=1)
-    max_pred[~mask] = -1
-    _, max_residue = max_pred.max(dim=0)
-    x[max_residue] = max_index[max_residue]
-    return design_protein(net, x, edge_index, edge_attr)
+def designProtein(net : torch.nn.Module, kw_x : str, dim : int, **kwargs):
+    
+    # Can only design a single protein at the time
+    assert len(kwargs[kw_x].shape) == 1
+
+    # Mask initially
+    mask = kwargs[kw_x] == AMINO_ACIDS_MAP[AMINO_ACID_NULL]
+
+    while not mask.any():
+
+        # Predict based on current predictions
+        output = net(**kwargs)
+
+        # Normalize predictions
+        output = torch.softmax(output, dim=dim)
+
+        # Get per residue max prediction
+        # Set already predictions to by -1 in order not to change them again 
+        max_pred, max_index = output.max(dim=dim)
+        max_pred[~mask] = -1
+        _, max_residue = max_pred.max(dim=0)
+
+        # Update with max prediction
+        kwargs[kw_x][max_residue] = max_index[max_residue]
+        mask[max_residue] = False
+    
+    return kwargs[kw_x]
