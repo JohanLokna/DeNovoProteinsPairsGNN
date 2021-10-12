cbd586e36863d804c139805748df69ae7f89b75c
Johan Lokna <jlokna@student-net.ethz.ch>
2021-10-12 20:15:19 +0200

    .

--- ProteinPairsGenerator/PostProcessing/guessesData.py
+++ ProteinPairsGenerator/PostProcessing/guessesData.py
@@ -16,23 +16,67 @@ from ProteinPairsGenerator.utils import seq_to_tensor, AMINO_ACIDS_MAP
 """
 class GuessDataset(torch.utils.data.IterableDataset):
 
-    def __init__(self, root : str, xToken : str = "guess", yToken : str = "seq", maskToken = "mask") -> None:
+    def __init__(self, root : str, batch_size = 0, xToken : str = "guess", yToken : str = "seq", maskToken = "mask") -> None:
         super().__init__()
         self.tokenizer = AdaptedTAPETokenizer()
         self.xToken = xToken
         self.yToken = yToken
         self.maskToken = maskToken
+        self.batch_size = batch_size
 
         self.data = []
+
+        # Iterate over dataset and create dataset with each 
+        # batch having roughly batch_size tokens
+        x_batch, y_batch, mask_batch = [], [], []
+        B, L = 0, 0
         for l in open(root, "r").readlines():
 
+            # Load line
             jsonDict = json.loads(l)
 
-            self.data.append(GeneralData(
-              x = seq_to_tensor(jsonDict[self.xToken], AMINO_ACIDS_MAP),
-              y = seq_to_tensor(jsonDict[self.yToken], AMINO_ACIDS_MAP),
-              mask = torch.BoolTensor(jsonDict[self.maskToken])
+            # Add sequence to batch
+            x_batch.append(jsonDict[self.xToken])
+            y_batch.append(jsonDict[self.yToken])
+            mask_batch.append(jsonDict[self.maskToken])
+
+            # Update size parameters
+            B += 1
+            L = max(L, len(x_batch[-1]))
+
+            # Test if batch is big enough
+            if B * L > batch_size:
+                self.data.append(
+                    self.combine_batch(B, L, x_batch, y_batch, mask_batch
+                ))
+                x_batch, y_batch, mask_batch = [], [], []
+                B, L = 0, 0
+
+        # Append last batch if not empty
+        if B > 0:
+            self.data.append(
+                self.combine_batch(B, L, x_batch, y_batch, mask_batch
             ))
+            
+
+    def combine_batch(self, B, L, x_batch, y_batch, mask_batch):
+
+        x = torch.zeros(B, L, dtype=torch.long)
+        y = torch.zeros(B, L, dtype=torch.long)
+        mask = torch.zeros(B, L, dtype=torch.bool)
+
+        for i in range(B):
+            length = len(x_batch[i])
+            x[i, :length] = seq_to_tensor(x_batch[i], AMINO_ACIDS_MAP)
+            y[i, :length] = seq_to_tensor(y_batch[i], AMINO_ACIDS_MAP)
+            mask[i, :length] = torch.BoolTensor(mask_batch[i])
+
+        return GeneralData(
+          x = x,
+          y = y,
+          mask = mask
+        )
+
 
     def __iter__(self):
         return iter(self.data)
@@ -64,12 +108,13 @@ class GuessDataModule(pl.LightningDataModule):
         trainSet: Optional[str] = None,
         valSet: Optional[str] = None,
         testSet: Optional[str] = None,
+        batch_size = 0
     ) -> None:
         super().__init__()
 
-        self.trainDataset = GuessDataset(trainSet) if trainSet else None
-        self.valDataset = GuessDataset(valSet) if valSet else None
-        self.testDataset = GuessDataset(testSet) if testSet else None
+        self.trainDataset = GuessDataset(trainSet, batch_size) if trainSet else None
+        self.valDataset = GuessDataset(valSet, batch_size) if valSet else None
+        self.testDataset = GuessDataset(testSet, batch_size) if testSet else None
 
     def setup(self, stage=None):
         pass
