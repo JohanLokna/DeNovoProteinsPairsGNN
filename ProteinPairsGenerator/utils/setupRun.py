bf640462925609daf65de318c6e53f061a7016ac
Johan Lokna <jlokna@student-net.ethz.ch>
2021-10-14 13:39:00 +0200

    .

--- ProteinPairsGenerator/utils/setupRun.py
+++ ProteinPairsGenerator/utils/setupRun.py
@@ -63,9 +63,7 @@ def setupRun(
     device = getFreeGPU()
 
     # Run
-    # os.system("export CUDA_VISIBLE_DEVICES={} ; cd {} ; python3 {} --config config.yaml >> out.out 2>&1".format(device.id, str(root), str(runFile)))
-    os.system("export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 ; cd {} ; python3 {} --config config.yaml >> out.out 2>&1".format(str(root), str(runFile)))
-
+    os.system("export CUDA_VISIBLE_DEVICES={} ; cd {} ; python3 {} --config config.yaml >> out.out 2>&1".format(device.id, str(root), str(runFile)))
 
     # Release lock on device
     locks[int(device.id)].release()
